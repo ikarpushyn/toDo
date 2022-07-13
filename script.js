a783ecb694ad723f4647062934306afc0204cb74
ilija <komperdel@gmail.com>
2022-07-13 13:56:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// setTimeout(() => {
+// 	const main = document.querySelector('main');
+// 	main.style.opacity = 1;
+// 	main.style.filter = 'blur(0px)';
+// }, 1000);
+
+const list = document.querySelector('.list');
+const checked = document.querySelector('.list');
+const closeList = document.getElementsByClassName('close');
+const addForm = document.querySelector('form.input_area');
+const addInput = document.querySelector('.adding__input');
+
+const toDoList = {
+	list: [],
+};
+
+addForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	let newList = addInput.value;
+
+	if (newList) {
+		if (newList.length > 20) {
+			newList = `${newList.substring(0, 21)}...`;
+		}
+
+		toDoList.list.push(newList);
+
+		list.innerHTML = '';
+
+		toDoList.list.forEach((listItem) => {
+			list.innerHTML += `
+			<li data-checked="check" class="list_item">
+			 ${listItem}
+				<div class="delete"></div>
+			</li>
+			`;
+		});
+	} else {
+		alert('write smth');
+	}
+
+	e.target.reset();
+});
+
+//delete when click on ico
+const trashBox = document.querySelectorAll('.delete');
+trashBox.forEach((elem, i) => {
+	elem.addEventListener('click', () => {
+		elem.parentElement.remove();
+		toDoList.list.splice(i, 1);
+	});
+});
+
+// create check effect
+checked.addEventListener(
+	'click',
+	(e) => {
+		if (e.target.tagName === 'LI') {
+			e.target.classList.toggle('checked');
+		}
+	},
+	false
+);
+
+console.log(list);
+console.log(toDoList.list);
